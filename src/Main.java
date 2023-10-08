6b2f2dc080768ac263e6c76ed04a130858ca8947
JambaWAT <Puskinalex38@gmail.com>
2023-10-08 03:06:50 +0300

    Сделал дз

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("Задача2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        System.out.println("Задача3");
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        System.out.println("Задача4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        System.out.println("Задача5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println("Задача6");
+        var boxer2Weight = 78.2;
+        var boxer1Weight = 82.7;
+        var averageWeight = (boxer2Weight + boxer1Weight) / 2;
+        System.out.println("Общая масса двух бойцов " + averageWeight + " кг");
+        var weightDifference3 = (boxer1Weight - boxer2Weight);
+        System.out.println("Разница масс спортсменов на " + weightDifference3 + " Кг");
+        System.out.println("Задача7");
+        var weightDifference1 = (boxer1Weight - boxer2Weight);
+        System.out.println("Разница масс спортсменов на " + weightDifference1 + " Кг");
+        var weightDifference2 = (boxer1Weight % boxer2Weight);
+        System.out.println("Разница масс спортсменов на " + weightDifference2 + " Кг");
+        System.out.println("Задача8");
+        var workHours = 640;
+        var hoursPerWorkers = 8;
+        var workerCount = workHours / hoursPerWorkers;
+        System.out.println("Всего работников в компании" + workerCount + " Человек");
+        var workerCount2 = workerCount + 94;
+        var workHours2 = workerCount2 * 8;
+        System.out.println("Если в компании работает " + workerCount2 + " Человек, то всго " + workHours2 + " часов работы может быть поделено между сотрудниками.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
